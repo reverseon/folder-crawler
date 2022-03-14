3a5d36fe5de9a9bd17dd52b80a0f7b14e2175b6c
ghebyon <13520079@std.stei.itb.ac.id>
2022-03-15 06:01:03 +0700

    Folder File Data in Dictionary

---
+++ FolderCrawler/FolderCrawler/FolderFileDictionary.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FolderCrawler
+{
+    class FolderFileDictionary
+    {
+        private const int RootLevel = 0;
+        private DirectoryInfo rootDirectory;
+        public Dictionary<string, List<string>> dir = new Dictionary<string, List<string>>();
+
+        public void CreateDirectoryTree(string rootDirectoryPath)
+        {
+            try
+            {
+                if (!Directory.Exists(rootDirectoryPath))
+                {
+                    throw new DirectoryNotFoundException(
+                        $"Directory '{rootDirectoryPath}' not found.");
+                }
+
+                this.rootDirectory = new DirectoryInfo(rootDirectoryPath);
+                CreateDirectoryTree(rootDirectory);
+
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private void CreateDirectoryTree(DirectoryInfo directory)
+        {
+            List<string> container = new List<string>();
+            foreach (var subDirectory in directory.GetDirectories())
+            {
+                container.Add(subDirectory.Name);
+                CreateDirectoryTree(subDirectory);
+            }
+            foreach (var file in directory.GetFiles())
+            {
+                container.Add(file.Name);
+            }
+            this.dir[directory.Name] = container;
+        }
+        public void PrintDictionaryTree()
+        {
+            Console.WriteLine(this.dir[this.rootDirectory.Name]);
+            PrintDictionaryTree(this.rootDirectory.Name, RootLevel);
+        }
+
+        private void PrintDictionaryTree(string directoryName, int currentLevel)
+        {
+            var indentation = string.Empty;
+            char Tab = '\t';
+            for (var i = RootLevel; i < currentLevel; i++)
+            {
+                indentation += Tab;
+            }
+
+            foreach (string value in this.dir[directoryName])
+            {
+                Console.WriteLine($"{indentation}-{value}");
+                if (this.dir.ContainsKey(value))
+                {
+                    PrintDictionaryTree(value, currentLevel + 1);
+                }
+            }
+        }
+    }
+}
---
+++ FolderCrawler/FolderCrawler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace FolderCrawler
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //Console.WriteLine("Enter the path of the root directory:");
+            var rootDirectorypath = "D:\\Kuliah\\Tingkat 2\\Semester 4\\IF2230 - Sistem Operasi\\Soal";
+
+            Console.WriteLine($"Getting directory tree of '{rootDirectorypath}'");
+            FolderCrawler.FolderFileDictionary d = new FolderFileDictionary();
+            d.CreateDirectoryTree(rootDirectorypath);
+            d.PrintDictionaryTree();
+        }
+
+    }
+}
