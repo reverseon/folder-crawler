b92a383c749c3dbbc7b132d087f2a7e76ef829b9
reverseon <reverseon@outlook.com>
2022-03-25 20:23:00 +0700

    Adding a Few Comments

--- FolderCrawler/folder-crawler/Form1.cs
+++ FolderCrawler/folder-crawler/Form1.cs
@@ -20,7 +20,7 @@ namespace folder_crawler
         string textboxplaceholder = "e.g. shenhebuild_2022.pdf";
         string startingpathGL = "";
         bool foundFirst = false;
-        bool interruptit = false;
+        bool interruptit = false; // for handling interrupt button (Cancel Search)
         int leftLatest = 0;
         int topLatest = -30;
         System.Threading.Mutex mutex = new System.Threading.Mutex(); 
@@ -30,17 +30,18 @@ namespace folder_crawler
         public Form1()
         {
             InitializeComponent();
-            tbox_filename.Text = textboxplaceholder;
-            btn_interrupt.Enabled = false;
+            tbox_filename.Text = textboxplaceholder; // init placeholder for textbox filename
+            btn_interrupt.Enabled = false; // cant interrupt at first
         }
         private void gViewer1_Load(object sender, EventArgs e)
         {
-            graphres = new Microsoft.Msagl.Drawing.Graph("graph");
+            graphres = new Microsoft.Msagl.Drawing.Graph("graph"); // initialize handler
 
         }
 
         private void btn_choosefolder_Click(object sender, EventArgs e)
         {
+            // choosing a start folder
             using (var fbd = new FolderBrowserDialog())
             {
                 DialogResult result = fbd.ShowDialog();
@@ -53,6 +54,7 @@ namespace folder_crawler
         }
         private void tbox_filename_clicked(object sender, EventArgs e)
         {
+            // inquiries for filename that need to be searched
             if (tbox_filename.Text == textboxplaceholder)
             {
                 tbox_filename.Text = "";
@@ -63,6 +65,7 @@ namespace folder_crawler
         }
         RadioButton GetCheckedRadio(Control container)
         {
+            // for getting which radiobutton is checked
             foreach (var control in container.Controls)
             {
                 RadioButton radio = control as RadioButton;
@@ -77,11 +80,11 @@ namespace folder_crawler
         }
         private void do_bfs(string startingpath, string filename, bool findall, BackgroundWorker worker, DoWorkEventArgs e)
         {
-            if (e.Cancel || interruptit || (!findall && foundFirst))
+            if (e.Cancel || interruptit || (!findall && foundFirst)) // stop when not findall and found it already, cancel the bgworker, or interrupt
             {
                 return;
             }
-            else if (worker.CancellationPending)
+            else if (worker.CancellationPending) // when pending, update the status
             {
                 e.Cancel = true;
                 interruptit = true;
@@ -95,10 +98,12 @@ namespace folder_crawler
                 string[] indifolder = toparse.Trim().Split(new[] { Path.DirectorySeparatorChar }, StringSplitOptions.RemoveEmptyEntries);
                 bool foundThisContext = false;
 
-                graphres.AddNode(startingpath);
+                graphres.AddNode(startingpath); // add parent node
                 if (startingpath == startingpathGL) graphres.FindNode(startingpath).Label.Text = startingpath;
                 else graphres.FindNode(startingpath).Label.Text = new DirectoryInfo(startingpath).Name;
-                
+
+            
+                // check file first    
                 foreach (string file in files)
                 {
 
@@ -110,7 +115,7 @@ namespace folder_crawler
                         graphres.FindNode(Path.Combine(startingpath, file)).Attr.Color = Microsoft.Msagl.Drawing.Color.Blue;
                         graphres.AddEdge(startingpath, Path.Combine(startingpath, file)).Attr.Color = Microsoft.Msagl.Drawing.Color.Blue;
                         
-                        // add label
+                        // add label below gview
                         if (pnl_folderloc.InvokeRequired)
                         {
                             pnl_folderloc.Invoke((MethodInvoker)delegate ()
@@ -148,7 +153,7 @@ namespace folder_crawler
                         }
                         if (!findall) break;
                     }
-                    else if (!(!findall && foundFirst))
+                    else if (!(!findall && foundFirst)) // basically when findall
                     {
                         graphres.AddNode(Path.Combine(startingpath, file)).Label.Text = file;
                         graphres.FindNode(Path.Combine(startingpath, file)).Attr.Color = Microsoft.Msagl.Drawing.Color.Red;
@@ -156,7 +161,7 @@ namespace folder_crawler
                         
                     }
                     else
-                    {
+                    { // not sure which case goes here but for safety measure here we go
                         graphres.AddNode(Path.Combine(startingpath, file)).Label.Text = file;
                         graphres.AddEdge(startingpath, Path.Combine(startingpath, file));
                     }
@@ -165,7 +170,7 @@ namespace folder_crawler
 
                 
 
-                if (!(!findall && foundFirst))
+                if (!(!findall && foundFirst)) // if findall, continue the searching to the folder
                 {
                     foreach (string file in allfolder)
                     {
@@ -177,7 +182,7 @@ namespace folder_crawler
                 // CRITICAL SECTION STARTED
 
 
-                if (foundThisContext)
+                if (foundThisContext) // if found in this context, start backtracking to color blue to appropriate edge
                 {
                     int i = 0;
                     graphres.FindNode(startingpathGL).Attr.Color = Microsoft.Msagl.Drawing.Color.Blue;
@@ -202,7 +207,7 @@ namespace folder_crawler
                         }
                     }
                 }
-                else
+                else // if not, then red
                 {
                     int i = 0;
                     if (graphres.FindNode(startingpathGL).Attr.Color != Microsoft.Msagl.Drawing.Color.Blue)
@@ -233,7 +238,7 @@ namespace folder_crawler
                 }
                 // CRITICAL SECTION ENDED
 
-                if (gViewer1.InvokeRequired)
+                if (gViewer1.InvokeRequired) // for update the gview
                 {
                     gViewer1.Invoke((MethodInvoker)delegate ()
                     {
@@ -250,7 +255,7 @@ namespace folder_crawler
         }
         private void do_dfs(string startingpath, string filename, bool findall, BackgroundWorker worker, DoWorkEventArgs e)
         {
-
+            // same as BFS
             if (e.Cancel || interruptit || (!findall && foundFirst))
             {
                 return;
@@ -272,7 +277,9 @@ namespace folder_crawler
                 graphres.AddNode(startingpath);
                 if (startingpath == startingpathGL) graphres.FindNode(startingpath).Label.Text = startingpath;
                 else graphres.FindNode(startingpath).Label.Text = new DirectoryInfo(startingpath).Name;
+                
 
+                // folder inspected first
                 foreach (string file in allfolder)
                 {
                     graphres.AddNode(Path.Combine(startingpath, file)).Label.Text = file;
@@ -280,10 +287,10 @@ namespace folder_crawler
                     do_dfs(Path.Combine(startingpath, file), filename, findall, worker, e);
                 }
 
-                foreach (string file in files)
+                foreach (string file in files) // file search
                 {
 
-                    if (file == filename && !(!findall && foundFirst))
+                    if (file == filename && !(!findall && foundFirst)) // prevent if not using findall 
                     {
                         foundFirst = true;
                         foundThisContext = true;
@@ -291,7 +298,7 @@ namespace folder_crawler
                         graphres.FindNode(Path.Combine(startingpath, file)).Attr.Color = Microsoft.Msagl.Drawing.Color.Blue;
                         graphres.AddEdge(startingpath, Path.Combine(startingpath, file)).Attr.Color = Microsoft.Msagl.Drawing.Color.Blue;
 
-                        // add label
+                        // add label update
                         if (pnl_folderloc.InvokeRequired)
                         {
                             pnl_folderloc.Invoke((MethodInvoker)delegate ()
@@ -343,6 +350,8 @@ namespace folder_crawler
                     }
                     /*gViewer1.Graph = graphres;*/
                 }
+
+                // backtrack coloring
                 if (foundThisContext)
                 {
                     int i = 0;
@@ -422,24 +431,25 @@ namespace folder_crawler
             string filename = tbox_filename.Text.Trim();
             bool findalloccurences = cbox_alloccurence.Checked;
             RadioButton method = GetCheckedRadio(method_panel_group);
-            if (method == null)
+            if (method == null) // not selecting a method
             {
                 MessageBox.Show("Method Not Selected", "Error");
             }
             else
-            if (filename.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+            if (filename.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0) // filename invalid
             {
                 MessageBox.Show("Invalid Filename", "Error");
             }
             else
-            if (startingpathGL == "None") MessageBox.Show("Starting Path Not Selected", "Error");
+            if (startingpathGL == "None") MessageBox.Show("Starting Path Not Selected", "Error"); // not selected a starting path
             else
-            if (startingpathGL.IndexOfAny(Path.GetInvalidPathChars()) >= 0)
+            if (startingpathGL.IndexOfAny(Path.GetInvalidPathChars()) >= 0) // starting path invalid
             {
                 MessageBox.Show("Invalid Starting Path", "Error");
             }
             else
             {
+                // timing feature
                 sw = new Stopwatch();
                 timer = new System.Timers.Timer(10);
                 timer.Elapsed += timer_Tick;
@@ -473,7 +483,7 @@ namespace folder_crawler
                 {
                     gViewer1.Graph = graphres;
                 }
-                List<object> workerargs = new List<object>();
+                List<object> workerargs = new List<object>(); // argument for background worker
                 workerargs.Add(method);
                 workerargs.Add(startingpathGL);
                 workerargs.Add(filename);
@@ -487,16 +497,16 @@ namespace folder_crawler
 
         private void btn_interrupt_Click(object sender, EventArgs e)
         {
-            bg_searching.CancelAsync();
-            interruptit = true;
+            bg_searching.CancelAsync(); // cancel bgworker
+            interruptit = true; // signal handler
         }
 
         private void bg_searching_DoWork(object sender, DoWorkEventArgs e)
         {
-            BackgroundWorker worker = sender as BackgroundWorker;
+            BackgroundWorker worker = sender as BackgroundWorker; // initiate bgworker
             List<object> workerArgs = e.Argument as List<object>;
             RadioButton r_method = workerArgs[0] as RadioButton;
-            if (r_method == radio_dfs)
+            if (r_method == radio_dfs) // do accordingly
             {
                 do_dfs((string)workerArgs[1], (string)workerArgs[2], (bool)workerArgs[3], worker, e);
             } else 
@@ -508,12 +518,13 @@ namespace folder_crawler
 
         private void bg_searching_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            if (e.Error != null)
+            if (e.Error != null) // if encounters error during work
             {
                 MessageBox.Show(e.Error.Message, "Error");
             } 
             sw.Stop();
             timer.Stop();
+            // stop the stopwatch
             if (lbl_time_elapsed_detail.InvokeRequired)
             {
                 lbl_time_elapsed_detail.Invoke((MethodInvoker)delegate
@@ -531,10 +542,11 @@ namespace folder_crawler
 
         private void bg_searching_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-
+            // nothing
         }
         private void timer_Tick(object sender, System.Timers.ElapsedEventArgs e)
         {
+            // update timer label
             if (lbl_time_elapsed_detail.InvokeRequired)
             {
                 lbl_time_elapsed_detail.Invoke((MethodInvoker)delegate
@@ -550,6 +562,7 @@ namespace folder_crawler
 
         private void link_lbl_click_handler(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            // for opening file link
             e.Link.Visited = true;
             LinkLabel tempo = (LinkLabel)sender;
             System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
